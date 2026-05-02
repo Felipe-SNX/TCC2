bd6a6d4b777c863da6dc00559b14d6a103f73d36
Rodolfo <rodolfo9254@gmail.com>
2026-05-01 23:13:04 -0300

    feat(ui): implementa snackbar global e substitui alertas no login

--- app-web/front/app/app.vue
+++ app-web/front/app/app.vue
@@ -3,5 +3,16 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+
+    <UiAppSnackbar
+      v-model="isVisible"
+      :message="message"
+      :color="color"
+      :timeout="timeout"
+    />
   </v-app>
 </template>
+
+<script setup lang="ts">
+const { isVisible, message, color, timeout } = useSnackbar()
+</script>
