345e8eea59edea18ff99acd5656f008d243e8c8b
Rodolfo <rodolfo9254@gmail.com>
2026-05-05 19:44:54 -0300

    feat: ajusat switch

--- app-web/front/app/components/layout/AppHeader.vue
+++ app-web/front/app/components/layout/AppHeader.vue
@@ -6,9 +6,11 @@
     height="60"
     style="z-index: 1000"
   >
-    <div class="px-6 w-100 d-flex flex-column justify-center">
-      <div class="text-h5 font-weight-bold">{{ title }}</div>
-      <small class="text-body-2" style="opacity: 0.8">{{ description }}</small>
+    <div class="px-6 w-100 d-flex align-center">
+      <div class="d-flex flex-column justify-center flex-grow-1">
+        <div class="text-h5 font-weight-bold">{{ title }}</div>
+        <small class="text-body-2" style="opacity: 0.8">{{ description }}</small>
+      </div>
     </div>
   </v-app-bar>
 </template>
