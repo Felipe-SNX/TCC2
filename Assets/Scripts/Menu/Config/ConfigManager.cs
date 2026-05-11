e6ecd982db6e3bd22c848c874ba29e026d5b0a11
FELIPE EDUARDO DOS SANTOS <fesantos@mppr.mp.pr>
2026-05-11 16:50:40 -0300

    inicio tela pause e passagem de configurações para um prefab (incompleto)

---
+++ Assets/Scripts/Menu/Config/ConfigManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ConfigManager : MonoBehaviour
+{
+
+    public void FecharConfiguracoes()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void MudarVolumeGeral(float valor)
+    {
+        //Lógicas internas
+    }
+}
