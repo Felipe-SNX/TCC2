8f810bdedffbeb214890f267e8baf76385e90ce3
FELIPE EDUARDO DOS SANTOS <fesantos@mppr.mp.pr>
2026-05-11 16:03:21 -0300

    inicio novas telas

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -3,45 +3,57 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    [SerializeField] private string nomeLevel;
+    [Header("Painéis Principais")]
     [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelSelecaoFases;
     [SerializeField] private GameObject painelMenuConfiguracoes;
-    [SerializeField] private GameObject painelMenuAudio;
     [SerializeField] private GameObject painelMenuCreditos;
 
-    public void Jogar()
+    [Header("Sub-painéis")]
+    [SerializeField] private GameObject painelMenuAudio; 
+    
+    public void CarregarFase(string nomeDaFase)
+    {
+        SceneManager.LoadScene(nomeDaFase);
+    }
+
+    public void AbrirSelecaoFases()
     {
         painelMenuInicial.SetActive(false);
         painelSelecaoFases.SetActive(true);
     }
 
-    public void AbrirConfigurações()
+    public void AbrirConfiguracoes()
     {
         painelMenuInicial.SetActive(false);
         painelMenuConfiguracoes.SetActive(true);
-    } 
-
-    public void FecharConfigurações()
-    {
-        painelMenuConfiguracoes.SetActive(false);
-        painelMenuInicial.SetActive(true);
-    } 
-
-    public void AbrirMenuAudio()
-    {
-        painelMenuAudio.SetActive(true);
     }
 
     public void AbrirMenuCreditos()
     {
+        painelMenuInicial.SetActive(false);
         painelMenuCreditos.SetActive(true);
     }
 
-    public void RetornarTelaInicial()
+    public void VoltarParaMenuInicial()
     {
         painelSelecaoFases.SetActive(false);
-        painelMenuInicial.SetActive(true);    
+        painelMenuConfiguracoes.SetActive(false);
+        painelMenuCreditos.SetActive(false);
+        painelMenuAudio.SetActive(false); 
+
+        painelMenuInicial.SetActive(true);
+    }
+
+    public void AbrirMenuAudio()
+    {
+        painelMenuAudio.SetActive(true);          
+    }
+
+    public void FecharMenuAudio()
+    {
+        painelMenuAudio.SetActive(false);         
+        painelMenuConfiguracoes.SetActive(true);  
     }
 
     public void SairJogo()
