5d563c37c39725391aea1688c00a68dd07dd68c2
CJarlos <carlosrochacr11@outlook.com>
2026-06-07 01:17:26 -0300

    Commit message: Added the Message Puzzle mechanic, which allows players to collect fragments of a message and piece them together to reveal a hidden message. This mechanic is implemented through the use of collectible fragments and a UI that.
    Added: - FragmentButton prefab for collecting message fragmentsq
    - EndPhaseTrigger to signal the end of the puzzle phase
    - MessageFragment script to represent individual fragments
    - MessagePuzzleUI to display collected fragments and the complete message
    
    Modified: - Updated Map_Green.unity to include the new puzzle elements
    - Updated MessageManager and CollectibleMessage to support the new mechanic

---
+++ Assets/Scripts/Sandbox/EndPhaseTrigger.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+public class EndPhaseTrigger : MonoBehaviour
+{
+    public MessagePuzzleUI puzzleUI;
+    public int totalFragmentsRequired = 3;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+        if (MessageManager.Instance == null)
+        {
+            Debug.LogError("MessageManager não encontrado!");
+            return;
+        }
+        if (!MessageManager.Instance.HasAllFragments(totalFragmentsRequired))
+        {
+            Debug.Log("Você ainda não coletou todos os fragmentos.");
+            return;
+        }
+        puzzleUI.OpenPuzzle(MessageManager.Instance.GetCollectedFragments());
+    }
+}
