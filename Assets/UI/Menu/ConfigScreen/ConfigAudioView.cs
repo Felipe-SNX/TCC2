2f842ad8a41daf0fa1402cbfc745fdd378627d33
FELIPE EDUARDO DOS SANTOS <fesantos@mppr.mp.pr>
2026-06-23 14:51:01 -0300

    (fix) correção númerica dos sliders

--- Assets/UI/Menu/ConfigScreen/ConfigAudioView.cs
+++ Assets/UI/Menu/ConfigScreen/ConfigAudioView.cs
@@ -148,11 +148,11 @@ namespace Assets.UI.Menu.ConfigScreen
 
                 if (label != null)
                 {
-                    label.text = Mathf.RoundToInt(slider.value / 10f).ToString();
+                    label.text = Mathf.RoundToInt(slider.value).ToString();
 
                     slider.RegisterValueChangedCallback(evt =>
                     {
-                        label.text = Mathf.RoundToInt(evt.newValue / 10f).ToString();
+                        label.text = Mathf.RoundToInt(evt.newValue).ToString();
                     });
                 }
             }
@@ -166,7 +166,7 @@ namespace Assets.UI.Menu.ConfigScreen
 
             if (label != null)
             {
-                label.text = Mathf.RoundToInt(slider.value / 10f).ToString();
+                label.text = Mathf.RoundToInt(slider.value).ToString();
             }
 
             var fillElement = slider.Q<VisualElement>("slider-fill");
