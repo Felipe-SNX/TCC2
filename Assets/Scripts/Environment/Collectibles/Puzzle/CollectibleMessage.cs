8c2d5088f278c4523b2c167b41b3ca386cf81896
Felipe-SNX <felipesantossnx@gmail.com>
2026-06-28 16:25:25 -0300

    (fix) correção bug com puzzle final de fase

--- Assets/Scripts/Environment/Collectibles/Puzzle/CollectibleMessage.cs
+++ Assets/Scripts/Environment/Collectibles/Puzzle/CollectibleMessage.cs
@@ -5,16 +5,6 @@ public class CollectibleMessage : MonoBehaviour
     [Header("Fragmento da Mensagem")]
     [SerializeField] private MessageFragment fragment;
 
-    [Header("Configuração de limpeza")]
-    [SerializeField] private float destroyDistanceBehindCamera = 10f;
-
-    private Camera mainCamera;
-
-    private void Start()
-    {
-        mainCamera = Camera.main;
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player"))
@@ -40,16 +30,5 @@ public class CollectibleMessage : MonoBehaviour
         }
 
         Destroy(gameObject);
-    }
-
-    private void Update()
-    {
-        if (mainCamera == null)
-            return;
-
-        if (transform.position.y < mainCamera.transform.position.y - destroyDistanceBehindCamera)
-        {
-            Destroy(gameObject);
-        }
     }
 }
